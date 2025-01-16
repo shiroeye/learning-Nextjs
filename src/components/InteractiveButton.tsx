4c2d1f56c4e0686ba25a1c2ed6adde1dcc382d4f
{shiroeye} <{o.shuki2468@gmail.com}>
2025-01-16 16:15:45 +0900

    added useState

--- src/components/InteractiveButton.tsx
+++ src/components/InteractiveButton.tsx
@@ -1,25 +1,33 @@
 "use client";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function InteractiveButton() {
+  // Destructuring 分割代入
+  const [foo, setFoo] = useState(0);
+  console.log("foo1 is now", foo);
+
+  useEffect(() => {
+    console.log("foo value updated to:", foo);
+  }, [foo]);
+
+  function countButton() {
+    setFoo((f) => f + 1);
+    console.log("foo2 is now", foo);
+  }
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
-    console.log("Changed background color to lightblue");
 
     return () => {
       document.body.style.backgroundColor = "";
-      console.log("Changed background color to default");
     };
   }, []);
+
   return (
-    <button
-      className="bg-red-500"
-      onClick={(e) => {
-        console.log(e.target);
-        alert("Hello!!");
-      }}
-    >
-      Button
-    </button>
+    <div>
+      <h1>{foo}</h1>
+      <button className="bg-blue-500" onClick={countButton}>
+        Button
+      </button>
+    </div>
   );
 }
