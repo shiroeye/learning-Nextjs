8da83551f3a9f2b2d5fe0f6e4a9c58d301157a1b
{shiroeye} <{o.shuki2468@gmail.com}>
2025-01-18 08:57:32 +0900

    finished projectgit add .

--- src/hooks/useBgBlue.tsx
+++ src/hooks/useBgBlue.tsx
@@ -1,7 +1,11 @@
+"use client";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function useBgBlue() {
-  //...
+  const router = useRouter();
+  console.log(router);
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
 
